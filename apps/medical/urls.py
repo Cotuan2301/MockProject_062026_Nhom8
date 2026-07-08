fa76dff22fd4d5271b3d9b4d9d7a3292be4d177d
LuongTienLam <tienlamluong183@gmail.com>
2026-07-08 17:07:43 +0700

    feat: Hoàn thiện giao diện SC033 Bedside Vitals

--- apps/medical/urls.py
+++ apps/medical/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'medical'
 urlpatterns = [
-
+    path('bedside-vitals/', views.bedside_vitals, name='bedside_vitals'),
 ]
--- apps/medical/views.py
+++ apps/medical/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def bedside_vitals(request):
+    """
+    SC033 - Bedside Vitals (Ghi nhận Sinh hiệu tại giường)
+    Dummy data based on Figma mockup
+    """
+    context = {
+        'active_menu': 'care_planning',
+        'resident_name': 'Robert Hayes',
+        'room_number': 'Room 204B',
+        'task_name': 'Vitals check',
+        'due_time': 'due 14:00',
+        'recorder_name': 'Marcus Rivera, CNA',
+        'recorder_time': '2026-07-02 14:05',
+    }
+    return render(request, 'medical/bedside_vitals.html', context)
