becb41186994e104723715dd62d94a30c4b722ee
LuongTienLam <tienlamluong183@gmail.com>
2026-07-14 21:50:18 +0700

    feat(sc035): Complete Cost/Billing

--- apps/medical/urls.py
+++ apps/medical/urls.py
@@ -1,7 +1,8 @@
+# pyrefly: ignore [missing-import]
 from django.urls import path
 from . import views
 
 app_name = 'medical'
 urlpatterns = [
-
+    path('cost-billing/', views.billing_panel, name='cost_billing_panel'),
 ]
